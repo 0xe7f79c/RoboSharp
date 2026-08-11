ecaa33ee9e6eab3078f6d6ffe38d48f95e1e9da5
ps11n1 <ssaini456123@gmail.com>
2026-08-11 02:23:07 -0700

    add ?sync

--- cogs/misc.py
+++ cogs/misc.py
@@ -1,6 +1,9 @@
+import discord
+from discord.app_commands import CommandSyncFailure
 from discord.ext import commands
 
 from bot import RSharp
+from cogs.utils.context import GuildContext
 
 SOURCE_CODE = 'https://github.com/0xe7f79c/RoboSharp'
 
@@ -10,6 +13,20 @@ class Misc(commands.Cog):
 
     def __init__(self, bot: RSharp) -> None:
         self.bot = bot
+
+    @commands.is_owner()
+    @commands.command()
+    async def sync(self, ctx: GuildContext) -> None:
+        """Synchronizes the bots commands to this guild."""
+        self.bot.tree.copy_global_to(ctx.guild)
+
+        try:
+            commands = await self.bot.tree.sync(ctx.guild)
+            return await ctx.reply(f'\N{OK HAND SIGN} Synchronized all app commands. ({len(commands)} total commands)')
+        except discord.Forbidden:
+            return await ctx.reply('\N{NO ENTRY} Could not sync. (No `applications.commands` scope in guild...)')
+        except CommandSyncFailure:
+            return await ctx.reply('\N{NO ENTRY} Could not sync. (There is an error in one of your commands...)')
 
 
 async def setup(bot: RSharp) -> None:
